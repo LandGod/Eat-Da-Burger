3124b87447ca9c6e9c8567670d908183a224fd4e
Dan Gold <tentativechaos@gmail.com>
2019-09-24 23:06:00 -0700

    Initial file structure.

---
+++ config/orm.js
@@ -0,0 +1,9 @@
+var connection = require('connection.js');
+
+var ORM = {
+    selectAll = function() {},
+    insertOne = function() {},
+    updataOne = function() {}
+}
+
+module.exports = ORM;
---
+++ server.js
@@ -0,0 +1,54 @@
+var express = require("express");
+var exphbs = require("express-handlebars");
+var mysql = require("mysql");
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "",
+  database: /*TODO: DATABASE NAME */
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Serve index.handlebars to the root route, populated with all quote data.
+app.get("/", function(req, res) {
+  connection.query(/*TODO: QUERY GOES HERE */, function(err, data) {
+    if (err) {
+      return res.status(500).end();
+    }
+
+    res.render("index", { /* TODO: Handlebars data */ });
+  });
+});
+
+var routes = require("/config/connectionjs");
+
+
+app.use(routes);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
